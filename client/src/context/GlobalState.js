84aa1611607f030ca8876974a060073b0ab1e3e3
Arthur <arthurd800@wp.pl>
2020-03-17 11:39:20 +0100

    created new reducers for loading transactions from db and for errors,component TransactionList loads data from mongoDB,added new models, controllers and route in the backend code

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,10 +1,14 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
+import axios from 'axios';
 
 // Initial state
 const initialState = {
-  transactions: []
-}
+  transactions: [],
+  error: null,
+  loading: true
+};
+
 
 // Create context
 export const GlobalContext = createContext(initialState);
@@ -14,6 +18,21 @@ export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
   // Actions
+  async function getTransactions()
+  {
+    try {
+      const res = await axios.get('/api/v1/transactions'); 
+      dispatch({
+        type: 'GET_TRANSACTIONS',
+        payload: res.data.data
+      });
+    } catch (err) { 
+      dispatch({
+        type: 'TRANSACTION_ERROR',
+        payload: err.response.data.error
+      });
+    }
+  }
   function deleteTransaction(id) {
     dispatch({
       type: 'DELETE_TRANSACTION',
@@ -31,7 +50,10 @@ export const GlobalProvider = ({ children }) => {
   return (<GlobalContext.Provider value={{
     transactions: state.transactions,
     deleteTransaction,
-    addTransaction
+    addTransaction,
+    getTransactions,
+    error: state.error,
+    loading: state.loading
   }}>
     {children}
   </GlobalContext.Provider>);
